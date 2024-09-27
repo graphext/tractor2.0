8e5cd29cf687ce754546fc8efa26690f91768e56
Jesús Enrique Rascón <blograso@gmail.com>
2024-09-27 14:48:28 +0200

    add enhanced table

--- src/lib/components/LiveTable.svelte
+++ src/lib/components/LiveTable.svelte
@@ -1,5 +1,8 @@
 <script>
     import { Tooltip } from "bits-ui";
+    import { ClipboardText } from "phosphor-svelte";
+    import { toast } from "svelte-sonner";
+    import { fly } from "svelte/transition";
 
     export let headers;
     export let rows;
@@ -9,7 +12,7 @@
     <div
         class="overflow-auto max-h-[300px] border border-base-content/30 rounded-btn table-zebra"
     >
-        <table class="table table-sm table-pin-rows">
+        <table class="table table-sm">
             <thead class="border-b border-base-content">
                 <tr class="font-bold">
                     <th>#</th>
@@ -24,7 +27,42 @@
                     <tr class="hover border border-base-content/10">
                         <td class="opacity-20 text-right">{rows.length - i}</td>
                         {#each row as cell}
-                            <td class="truncate max-w-[500px]">{cell}</td>
+                            <td
+                                on:click={() => {
+                                    toast.success(
+                                        "Copied cell value to clipboard",
+                                    );
+                                    navigator.clipboard.writeText(cell);
+                                }}
+                            >
+                                <Tooltip.Root disableHoverableContent={true}>
+                                    <Tooltip.Trigger
+                                        class="max-w-[300px] truncate"
+                                    >
+                                        {cell}
+                                    </Tooltip.Trigger>
+                                    <Tooltip.Content
+                                        transition={fly}
+                                        transitionConfig={{
+                                            duration: 100,
+                                            y: 5,
+                                        }}
+                                    >
+                                        <div
+                                            class="flex gap-3 shadow-md items-center bg-primary text-white px-3 py-1 text-sm z-[9999] max-w-[800px] rounded-box"
+                                        >
+                                            <ClipboardText
+                                                size={15}
+                                                class="shrink-0"
+                                                weight="bold"
+                                            />
+                                            <div class="truncate">
+                                                {cell}
+                                            </div>
+                                        </div>
+                                    </Tooltip.Content>
+                                </Tooltip.Root>
+                            </td>
                         {/each}
                     </tr>
                 {/each}
