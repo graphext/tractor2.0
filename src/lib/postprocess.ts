e5f82993b57198efe38feaee5d9a3168f6019c08
Jesús Enrique Rascón <blograso@gmail.com>
2024-11-11 10:34:12 +0100

    remove old, unnecesary code

--- src/lib/postprocess.ts
+++ src/lib/postprocess.ts
@@ -1,57 +1,51 @@
 const tweetTypeMap = {
-	createdAt: "date",
-	text: "text",
-	url: "url",
-	viewCount: "number",
-	retweetCount: "number",
-	replyCount: "number",
-	likeCount: "number",
-	quoteCount: "number",
-	lang: "category",
-	bookmarkCount: "number",
-	source: "category",
-	isReply: "boolean",
-	isRetweet: "boolean",
-	isQuote: "boolean",
-	media: "list[url]",
+  createdAt: "date",
+  text: "text",
+  url: "url",
+  viewCount: "number",
+  retweetCount: "number",
+  replyCount: "number",
+  likeCount: "number",
+  quoteCount: "number",
+  lang: "category",
+  bookmarkCount: "number",
+  source: "category",
+  isReply: "boolean",
+  isRetweet: "boolean",
+  isQuote: "boolean",
+  media: "list[url]",
 };
 
 const tweetAuthorMap = {
-	authorName: "category",
-	authorUserName: "category",
-	authorUrl: "url",
-	authorFollowers: "number",
-	authorCreatedAt: "date",
-	authorIsVerified: "boolean",
-	authorProfilePicture: "url",
-	authorCoverPicture: "url",
-	authorDescription: "text",
-	authorLocation: "category",
+  authorName: "category",
+  authorUserName: "category",
+  authorUrl: "url",
+  authorFollowers: "number",
+  authorCreatedAt: "date",
+  authorIsVerified: "boolean",
+  authorProfilePicture: "url",
+  authorCoverPicture: "url",
+  authorDescription: "text",
+  authorLocation: "category",
 };
 
 export function createFunctionString() {
-	return `(object) => { const { author, ${Object.keys(tweetTypeMap).join(", ")} } = object; return { ${Object.keys(
-		tweetTypeMap,
-	)
-		.map((e) => '"' + e + "<gx:" + tweetTypeMap[e] + ">" + '": ' + e)
-		.join(
-			", ",
-		)}, "first_media_element<gx:url>": media[0], ${Object.keys(tweetAuthorMap).map((e) => '"' + e + "<gx:" + tweetAuthorMap[e] + ">" + '": ' + "author." + e.slice(6).charAt(0).toLowerCase() + e.slice(7))} }; }`;
-}
-
-const gNewsTypeMap = {
-	title: "category",
-	link: "url",
-	source: "category",
-	sourceUrl: "url",
-	publishedAt: "date",
-	loadedUrl: "url",
-	rssLink: "url",
-	image: "url",
-};
-
-// this makes no sense cause it depends on the actor, not apify.
-// the customMapFunction is only supported in the twitter actor
-export function gNewsMapFunction() {
-	return `(object) => return { ${Object.keys(gNewsTypeMap).map((e) => '"' + e + "<gx:" + gNewsTypeMap[e] + ">" + '": ' + e)} }`;
+  return `(object) => { const { author, ${Object.keys(tweetTypeMap).join(
+    ", "
+  )} } = object; return { ${Object.keys(tweetTypeMap)
+    .map((e) => '"' + e + "<gx:" + tweetTypeMap[e] + ">" + '": ' + e)
+    .join(", ")}, "first_media_element<gx:url>": media[0], ${Object.keys(
+    tweetAuthorMap
+  ).map(
+    (e) =>
+      '"' +
+      e +
+      "<gx:" +
+      tweetAuthorMap[e] +
+      ">" +
+      '": ' +
+      "author." +
+      e.slice(6).charAt(0).toLowerCase() +
+      e.slice(7)
+  )} }; }`;
 }
