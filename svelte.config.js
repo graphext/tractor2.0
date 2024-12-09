6511b5f91133277528b387f8e7b21d89ce39a4aa
Jesús Enrique Rascón <blograso@gmail.com>
2024-12-09 09:34:02 +0100

    change phosphor config to vite instead of svelte

--- svelte.config.js
+++ svelte.config.js
@@ -1,12 +1,11 @@
 import adapter from "@sveltejs/adapter-auto";
 import { vitePreprocess } from "@sveltejs/vite-plugin-svelte";
-import { sveltePhosphorOptimize } from "phosphor-svelte/vite";
 
 /** @type {import('@sveltejs/adapter-vercel').Config} */
 const config = {
 	// Consult https://kit.svelte.dev/docs/integrations#preprocessors
 	// for more information about preprocessors
-	preprocess: [sveltePhosphorOptimize(), vitePreprocess()],
+	preprocess: [vitePreprocess()],
 
 	kit: {
 		// adapter-auto only supports some environments, see https://kit.svelte.dev/docs/adapter-auto for a list.
